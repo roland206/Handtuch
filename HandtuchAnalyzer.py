2805b62efe2323a4718b4890b25b6c2288d5fe1e
Roland <roland.proksa@gmx.de>
2024-02-28 10:33:32 +0100

    Mit Korrelation

--- HandtuchAnalyzer.py
+++ HandtuchAnalyzer.py
@@ -9,25 +9,109 @@ from Reporter import *
 from Events import *
 import glob
 
+correlationNames = ['Verdunstung', "Temperatur", 'Luftfeuchte', 'Gewicht', 'Lüfter']
 class HandtuchAnalyzer(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, logPath, parent=None):
         QWidget.__init__(self, parent)
+        self.logPath = logPath + 'Handtuch_*.log'
+        self.selectedLogFiles = None
+        self.useSelection = False
         self.events = None
-        layout = QHBoxLayout()
+        self.maxLogFiles = 10
+        self.correlation = 0
+        self.corrPlot = Plot()
         self.plot = Plot()
+        layout = QHBoxLayout()
+        leftFrame = QFrame()
+        leftLayout = QVBoxLayout()
+        leftFrame.setLayout(leftLayout)
+        leftFrame.setMaximumWidth(1000)
+        leftLayout.addWidget(self.cmdFrame())
+        leftLayout.addWidget(self.corrPlot)
+        layout.addWidget(leftFrame)
         layout.addWidget(self.plot)
         self.setLayout(layout)
+        self.redraw()
 
-    def loadLogFiles(self, path, maxFile):
-        files = glob.glob(path + 'Handtuch_*.log')
+    def loadLogFiles(self):
+        if self.useSelection:
+            files = self.selectedLogFiles
+        else:
+            files = glob.glob(self.logPath)
         files.sort()
         self.events = createEvents()
-        if len(files) > maxFile:
-            files = files[-maxFile:]
+        if len(files) > self.maxLogFiles:
+            files = files[-self.maxLogFiles:]
         for file in files:
             loadEventFile(self.events, file)
         self.t0, self.t1 = timeSpan(self.events)
         self.redraw()
+
+    def daysChanged(self, i):
+        self.maxLogFiles = i
+        self.loadLogFiles()
+
+    def selectFiles(self):
+        files = QFileDialog.getOpenFileNames(self,'Öffne Log Files', '', 'Log Files (*.log);; Alle Dateien (*)')
+        if files:
+            self.selectedLogFiles = files[0]
+            self.useSelection = True
+            self.loadLogFiles()
+
+    def setLogType(self, selection):
+        self.useSelection = selection
+        if selection and self.selectedLogFiles is None:
+            self.selectFiles()
+        else:
+            self.loadLogFiles()
+    def cmdFrame(self):
+        frame = QFrame()
+        layout = QFormLayout()
+        frame.setLayout(layout)
+
+        historyBtn = QRadioButton('Aktuelle Historie')
+        historyBtn.setChecked(True)
+        selectedBtn = QRadioButton('Selektierte Log-files')
+        layout.addRow(historyBtn)
+        layout.addRow(selectedBtn)
+        historyBtn.toggled.connect(lambda:self.setLogType(False))
+        selectedBtn.toggled.connect(lambda:self.setLogType(True))
+
+        layout.addRow(QLabel('Anzahl Tage Rückschau'))
+        daysBtn = QSpinBox()
+        daysBtn.setValue(self.maxLogFiles)
+        daysBtn.setMinimum(1)
+        daysBtn.setMaximum(31)
+        daysBtn.valueChanged.connect(self.daysChanged)
+        daysBtn.setMaximumWidth(200)
+        layout.addRow(daysBtn)
+
+        selectBtn = QPushButton('Log files selektieren')
+        selectBtn.clicked.connect(self.selectFiles)
+        layout.addRow(selectBtn)
+
+
+        grid = QGroupBox("Korrelationen")
+        gridLayout = QGridLayout()
+        grid.setLayout(gridLayout)
+        layout.addRow(grid)
+
+
+        self.comboX = QComboBox(self)
+        self.comboY = QComboBox(self)
+        self.comboX.addItems(correlationNames)
+        self.comboY.addItems(correlationNames)
+        self.comboX.setCurrentIndex(0)
+        self.comboY.setCurrentIndex(1)
+        self.comboX.currentIndexChanged.connect(self.redraw)
+        self.comboY.currentIndexChanged.connect(self.redraw)
+        gridLayout.addWidget(QLabel('X-Achse'), 0, 0)
+        gridLayout.addWidget(QLabel('Y-Achse'), 1, 0)
+        gridLayout.addWidget(self.comboX, 0, 1)
+        gridLayout.addWidget(self.comboY, 1, 1)
+        frame.setMaximumWidth(500)
+        return frame
+
     def mousePressEvent(self, event) -> None:
         event.accept()
         self.lastPos = event.localPos().x()
@@ -56,7 +140,30 @@ class HandtuchAnalyzer(QWidget):
         self.t1 = min(center + span/2, time())
         self.redraw()
 
+    def getCorrelationData(self, index, verdunstung, t):
+        if index == 0: return verdunstung, None, '{0:4.1f}'
+        if index == 1: return self.events['T'].sampleData(t), None, '{0:4.1f}'
+        if index == 2: return self.events['H'].sampleData(t), None, '{0:4.1f}'
+        if index == 3: return self.events['G'].sampleData(t), None, '{0:4.1f}'
+        if index == 4:
+            raw = self.events['S'].sampleData(t)
+            raw = (raw.astype(int) >> 2) & 3
+            return raw, [-0.1, 3.1], '{0:3.1f}'
+        return None, None
+    def redrawCorrelation(self, time, data):
+        ix = self.comboX.currentIndex()
+        iy = self.comboY.currentIndex()
+        plot = self.corrPlot
+        plot.clr()
+        sp = plot.addSubPlot(1, title=f'Korrelation {correlationNames[ix]} / {correlationNames[iy]}')
+        datax, limitx, formatx = self.getCorrelationData(ix, data, time)
+        datay, limity, formaty = self.getCorrelationData(iy, data, time)
 
+        sp.symbol = '*'
+        sp.plot(datax, datay, xFormat=formatx, yFormat=formaty)
+        sp.setXlim(limitx)
+        sp.setYlim(limity)
+        plot.repaint()
     def redraw(self):
         if self.events is None: return
         plot = self.plot
@@ -115,11 +222,9 @@ class HandtuchAnalyzer(QWidget):
                 sp.timeAxis = True
                 sp.plot(t, d, colorIndex=2, yFormat='{0:6.3f}')
                 sp.plot(ts, ds, colorIndex=0)
-
+                if self.correlation  >= 0: self.redrawCorrelation(ts, ds)
         plot.setXlim([self.t0, self.t1+1])
         plot.repaint()
-
-
 
     def verdunstung(self, gewicht, status, mask):
         tOut, dOut = None, None
